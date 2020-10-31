58bb9a9850add8137e4fe4faeda449eed19f3fcd
katarinavuknic <katarinavuknic12345@gmail.com>
2020-10-31 16:08:23 +0100

    gatsby project clean up

---
+++ gatsby-project/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const IndexPage = () => (
+  <main>
+    <h1>Hi, user!</h1>
+    <p>Welcome to new Gatsby site.</p>
+    <p>This is were we will add content...</p>
+  </main>
+)
+
+export default IndexPage

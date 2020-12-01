9c0449fe30df35973949c1d3dae872162da3a671
unknown <mbrkic00@fesb.hr>
2020-12-01 13:35:04 +0100

    cleaning main page

--- gatsby-project/src/pages/index.js
+++ gatsby-project/src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react"
 
+
 const IndexPage = () => (
   <main>
-    <h1>Hi, user!</h1>
-    <p>Welcome to new Gatsby site.</p>
-    <p>This is were we will add content...</p>
+    
   </main>
 )
 

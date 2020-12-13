578ca5bab9c0b0158c2f91360bd459a32a346ce1
katarinavuknic <katarinavuknic12345@gmail.com>
2020-12-13 15:38:57 +0100

    adding hello message for user

---
+++ gatsby-project/src/components/MsgToUser/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const user = 'korisnik1203';
+function getGreeting(user) {
+    if (user) {
+      return <h1>Dobar dan, {user}!</h1>;
+    }
+    return <h1>Hello, newUser!</h1>;
+  }
+
+const MsgToUser = () => (
+    <main className={styles.msgToUser}>
+        <span className={styles.msgText}>{getGreeting(user)}</span>
+    </main>
+
+)
+
+export default MsgToUser
--- gatsby-project/src/components/Navigation/index.js
+++ gatsby-project/src/components/Navigation/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import NavigationBar from '../NavigationBar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-
+import MsgToUser from '../MsgToUser'
 import styles from './style.module.css'
+
 
 
 const Navigation = () => (
     <section className={styles.navigation}>
     <NavigationBar/>
     <FontAwesomeIcon icon={faShoppingCart} className={styles.ShoppingCart}/>
+    <MsgToUser/>
     </section>
 )
 

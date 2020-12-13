d0b3764cb2e8871266e09485faaca8c17e8a56ac
katarinavuknic <katarinavuknic12345@gmail.com>
2020-12-13 15:11:57 +0100

    adding shopping cart icon

--- gatsby-project/src/components/Navigation/index.js
+++ gatsby-project/src/components/Navigation/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import NavigationBar from '../NavigationBar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import styles from './style.module.css'
 
 
 const Navigation = () => (
     <section className={styles.navigation}>
     <NavigationBar/>
+    <FontAwesomeIcon icon={faShoppingCart} className={styles.ShoppingCart}/>
     </section>
 )
 

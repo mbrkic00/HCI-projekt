2ad4056b417c60a021f0896a9adbb29781d35c92
katarinavuknic <katarinavuknic12345@gmail.com>
2020-12-13 13:42:14 +0100

    adding navigation

---
+++ gatsby-project/src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+
+const Navigation = () => (
+    <section className={styles.navigation}>
+        
+    </section>
+)
+
+export default Navigation

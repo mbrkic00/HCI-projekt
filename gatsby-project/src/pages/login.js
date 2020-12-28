9964247f8b3f7befe8b94052998c991692d591e7
katarinavuknic <katarinavuknic12345@gmail.com>
2020-12-28 19:02:57 +0100

    page login init

---
+++ gatsby-project/src/pages/login.js
@@ -0,0 +1,9 @@
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter"
+ 
+const loginPage = () => (
+    <HeaderFooterLayout activeTab="Prijavi se">
+    </HeaderFooterLayout>
+)
+ 
+export default loginPage

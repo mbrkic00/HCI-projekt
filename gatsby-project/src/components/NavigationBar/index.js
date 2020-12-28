6d2fc36c899564f4d88161463314d82de4e62cc3
mbrkic00 <mbrkic00.fesb.hr>
2020-12-28 14:55:17 +0100

    adding buttons

--- gatsby-project/src/components/NavigationBar/index.js
+++ gatsby-project/src/components/NavigationBar/index.js
@@ -4,10 +4,10 @@ import styles from './style.module.css'
 
 
 const navTabs = ['Naslovna', 'Web shop', 'Blog', 'Prijavi se']
-const NavigationBar = () => (
+const NavigationBar = ({activeTab}) => (
     <nav className={styles.navigationBar}>
-        {navTabs.map(
-            tab => <li>{tab}</li>
+        {navTabs.map(tab => <li className={tab == activeTab ? styles.active : ''}> 
+            {tab}</li>
         )}
     </nav>
 )

82eb35546bf1c2c520b6ce847973c02ec70a56c6
katarinavuknic <katarinavuknic12345@gmail.com>
2020-12-28 15:31:25 +0100

    creating layout

--- gatsby-project/src/pages/index.js
+++ gatsby-project/src/pages/index.js
@@ -1,16 +1,11 @@
 import React from "react"
-import NavigationHeader from '../components/NavigationHeader'
-import TitleImage from "../components/TitleImage"
-import Footer from '../components/Footer'
-import FrontPage from '../components/FrontPage'
+ 
+import HeaderFooterLayout from "../layouts/headerFooter"
+ 
 const IndexPage = () => (
-  <main>
-    {/*<p style={{paddingLeft: '3rem'}}>Online cvjećarnica u izradi...</p>*/}
-    <NavigationHeader activeTab = "Naslovna"/>
-    <TitleImage/>
-    <FrontPage/>
-    <Footer/>
-  </main>
+  <HeaderFooterLayout activeTab="Naslovna">
+    {/* Body components here! */}
+  </HeaderFooterLayout>
 )
-
+ 
 export default IndexPage

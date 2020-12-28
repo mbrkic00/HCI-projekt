cf0830ce113a64fa2ad360d076122df9ccfbb27e
katarinavuknic <katarinavuknic12345@gmail.com>
2020-12-28 16:27:58 +0100

    adding roses image in layout

--- gatsby-project/src/layouts/headerFooter.js
+++ gatsby-project/src/layouts/headerFooter.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import NavigationHeader from '../components/NavigationHeader'
-import Footer from '../components/Footer'
+import NavigationHeader from '../modules/NavigationHeader'
+import TitleImage from '../modules/TitleImage'
+import Footer from '../modules/Footer'
  
 const HeaderFooterLayout = ({children, activeTab}) => (
   <>
     <NavigationHeader activeTab={activeTab} />
       {children}
+      <TitleImage/>
     <Footer />
   </>
 )

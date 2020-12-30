078bf0d4a0ec8736025aab08fa9d9169b2e79b5e
katarinavuknic <katarinavuknic12345@gmail.com>
2020-12-30 19:27:24 +0100

    second row images added and display resolved

--- gatsby-project/src/components/WebShopMain/index.js
+++ gatsby-project/src/components/WebShopMain/index.js
@@ -2,23 +2,31 @@ import React from 'react'
 import styles from './style.module.css'
 
 const WebShopMain = () => (
-    <webShopPage>
+    <webShopPage className={styles.webShop}>
         <div>
             <p className={styles.webTitle}>Web shop</p>
             <p className={styles.webMsg}>Odaberite željenu kategoriju i uživajte u kupovini!</p>
         </div>
         <section className={styles.categories}>
             <div className={styles.flowerCategory}>
-                <section className={styles.image1}></section>
+                <div className={styles.image1}></div>
                 <button className={styles.openButton}><span>Buketi (15)</span></button>
             </div>
             <div className={styles.flowerCategory}>
-                <section className={styles.image2}></section>
+                <div className={styles.image2}></div>
                 <button className={styles.openButton}><span>Cvijeće u kutiji (10)</span></button>
             </div>
             <div className={styles.flowerCategory}>
-                <section className={styles.image3}></section>
+                <div className={styles.image3}></div>
                 <button className={styles.openButton}><span>Aranžmani (7)</span></button>
+            </div>
+            <div className={styles.flowerCategory}>
+                <div className={styles.image4}></div>
+                <button className={styles.openButton}><span>Lončanice (7)</span></button>
+            </div>
+            <div className={styles.flowerCategory}>
+                <div className={styles.image5}></div>
+                <button className={styles.openButton}><span>Izradi po želji (7)</span></button>
             </div>
             
         </section>

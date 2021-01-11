8b68536cb6e9dc9d125ab75c1d1aede0fac9a76c
katarinavuknic <katarinavuknic12345@gmail.com>
2021-01-12 00:50:29 +0100

    adding quantity box looks

--- gatsby-project/src/components/webShop3_LjiljaniIRuze/index.js
+++ gatsby-project/src/components/webShop3_LjiljaniIRuze/index.js
@@ -12,7 +12,11 @@ const WebShop3_LjiljaniIRuze = () => (
                 <p className={styles.title}>Opis proizvoda:</p>
                 <div className={styles.description}>Buket sadrži ruže, ljiljane, gumbeke i dekorativno zelenilo, umotan u elegantan papir sa satenskom mašnom.</div>
                 <p className={styles.price}>200.00 kn</p>
-                <div className={styles.quantity}></div>
+                <div className={styles.quantity}>
+                    <input type="button" value="-" className={styles.minus}></input>
+                    <input type="number" step="1" min="1" max="" value="1" className={styles.numBox}></input>
+                    <input type="button" value="+" className={styles.plus}></input>
+                </div>
                 <button className={styles.addButton}><span>Dodaj u košaricu</span></button>
             </div>
         </div>

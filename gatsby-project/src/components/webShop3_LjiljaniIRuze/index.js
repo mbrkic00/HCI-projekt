3e7f2817061e64890fba861a8c8c094cfb165deb
katarinavuknic <katarinavuknic12345@gmail.com>
2021-01-12 12:54:02 +0100

    adding suggestion gallery part2

--- gatsby-project/src/components/webShop3_LjiljaniIRuze/index.js
+++ gatsby-project/src/components/webShop3_LjiljaniIRuze/index.js
@@ -22,11 +22,26 @@ const WebShop3_LjiljaniIRuze = () => (
         </div>
         <p className={styles.suggestionTitle}>Možda Vam se sviđa:</p>
         <div className={styles.suggestionsGallery}>
-            <div className={styles.galleryImage}></div>
-            <div className={styles.galleryImage}></div>
-            <div className={styles.galleryImage}></div>
-            <div className={styles.galleryImage}></div>
-            <div className={styles.galleryImage}></div>
+            <div className={styles.suggestionBox}>
+                <img src={require("../../images/buketi2.jpg")} alt="img001" className={styles.galleryImage}></img>
+                <p className={styles.imageName}>Jednostavne ruže</p>
+            </div>
+            <div className={styles.suggestionBox}>
+                <img src={require("../../images/buketi3.jpg")} alt="img001" className={styles.galleryImage}></img>
+                <p className={styles.imageName}>Nježne orhideje</p>
+            </div>
+            <div className={styles.suggestionBox}>
+                <img src={require("../../images/buketi4.jpg")} alt="img001" className={styles.galleryImage}></img>
+                <p className={styles.imageName}>Egzotični mix</p>
+            </div>
+            <div className={styles.suggestionBox}>
+                <img src={require("../../images/buketi5.jpg")} alt="img001" className={styles.galleryImage}></img>
+                <p className={styles.imageName}>Šareni tulipani</p>
+            </div>
+            <div  className={styles.suggestionBox}>
+                <img src={require("../../images/buketi6.jpg")} alt="img001" className={styles.galleryImage}></img>
+                <p className={styles.imageName}>Šareni buket</p>
+            </div>
         </div>
     </div>
 )

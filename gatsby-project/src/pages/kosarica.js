d3c6ce1ce4b7f9eab46b905d82c2f11cad4b83b7
mbrkic00 <mbrkic00.fesb.hr>
2021-01-13 13:02:46 +0100

    adding kosarica page

---
+++ gatsby-project/src/pages/kosarica.js
@@ -0,0 +1,12 @@
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter"
+// import ShoppingChart from "../components/ShoppingChart"
+ 
+const shoppingChartPage = () => (
+    <HeaderFooterLayout>
+        {/* <ShoppingChart/>  */}
+    </HeaderFooterLayout>
+    
+)
+ 
+export default shoppingChartPage

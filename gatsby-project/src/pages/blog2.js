fe25d28e579efa0dc7e6460f5f7e60c43a106b2e
mbrkic00 <mbrkic00.fesb.hr>
2021-01-14 12:19:59 +0100

    adding BlogPageNews2

---
+++ gatsby-project/src/pages/blog2.js
@@ -0,0 +1,12 @@
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter"
+import BlogPageNews2 from "../components/BlogPageNews2"
+ 
+const blogPage = () => (
+    <HeaderFooterLayout activeTab="Blog">
+        <BlogPageNews2 />
+    </HeaderFooterLayout>
+    
+)
+ 
+export default blogPage

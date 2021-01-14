33ee54a898a933bfac63ecd283a9092b75c4d53b
mbrkic00 <mbrkic00.fesb.hr>
2021-01-14 15:38:02 +0100

    adding another frame on ShoppingChart page

--- gatsby-project/src/components/ShoppingChart/index.js
+++ gatsby-project/src/components/ShoppingChart/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './style.module.css'
 import { InChart } from '../../constants/const'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const ShoppingChart = () => (
     <shoppingchartpage>
@@ -11,7 +11,8 @@ const ShoppingChart = () => (
         <div className={styles.h2}>Proizvodi u Vašoj košarici</div>
         </span>
 
-        <section className={styles.frame}>
+    <section className={styles.frames}>
+        <section className={styles.frame1}>
             <div className={styles.description}>
                 <p className={styles.price}>cijena</p>
                 <p className={styles.quantity}>količina</p> 
@@ -32,6 +33,26 @@ const ShoppingChart = () => (
             </div>
 
         </section>
+
+        <section className={styles.frame2}>
+            <div className={styles.total}>Ukupan iznos</div>
+            <div className={styles.productPrice}>
+                <p>Cijena proizvoda:</p>
+                <p className={styles.pP1}>715.00 kn</p>
+            </div>
+            <div className={styles.shipping}>
+                <p>Trošak dostave:</p>
+                <p className={styles.pP2}>0.00 kn</p>
+            </div>
+            <div className={styles.billing}>
+                <p>Ukupno za naplatu:</p>
+                <p className={styles.pP3}>715.00 kn</p>
+            </div>
+            <div className={styles.buttonStyle}><button className={styles.continue}>Nastavi<FontAwesomeIcon icon={faChevronRight} className={styles.chevronIcon}/></button></div>
+            <div className={styles.delivery}>Za sigurnu dostavu brine se HP Express/Overseas.</div>
+            <div className={styles.creditCards}></div>
+        </section>
+    </section>
 
     </shoppingchartpage>
 )

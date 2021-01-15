663d26b940b0240db6499ac1bdb93bcd6ff69517
mbrkic00 <mbrkic00.fesb.hr>
2021-01-15 12:57:58 +0100

    editing login page

--- gatsby-project/src/pages/login.js
+++ gatsby-project/src/pages/login.js
@@ -1,9 +1,11 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import LogIn from '../components/LogIn'
+import FrontPage from '../components/FrontPage'
  
 const loginPage = () => (
     <HeaderFooterLayout activeTab="Prijavi se">
+        <FrontPage/>
         <LogIn/>
     </HeaderFooterLayout>
 )

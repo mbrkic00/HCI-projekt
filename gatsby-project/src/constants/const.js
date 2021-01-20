129b4ed80368d2a0cf3afabeee7370c9dff8ca6d
katarinavuknic <katarinavuknic12345@gmail.com>
2021-01-20 12:14:39 +0100

    adding descriptions of products

--- gatsby-project/src/constants/const.js
+++ gatsby-project/src/constants/const.js
@@ -46,30 +46,35 @@ export const productsBuketi =[
         productName: "Ljiljani i ruže mix",
         productPrice: "175.00 kn",
         productImage: require("../../src/images/buketi1.jpg"),
+        productDescription: "Buket sadrži ljiljan, ruže, gumbeke, dekorativno zelenilo, umotan u ukrasni papir sa satenskom mašnom.",
         to: '/ljiljaniIRuze'
     },
     {
         productName: "Jednostavne ruže",
         productPrice: "250.00 kn",
         productImage: require("../../src/images/buketi2.jpg"),
+        productDescription:"Buket sadrži ruže, dekorativno zelenilo, ukrasni til i satensku mašnu.",
         to: '/'
     },
     {
         productName: "Nježne orhideje",
         productPrice: "210.00 kn",
         productImage: require("../../src/images/buketi3.jpg"),
+        productDescription:"Buket sadrži granu orhideje, dekorativno zelenilo i ukrasni til.",
         to: '/'
     },
     {
         productName: "Egzotični mix",
         productPrice: "300.00 kn",
         productImage: require("../../src/images/buketi4.jpg"),
+        productDescription: "Buket sadrži ruže, mini ruže, irise, ljiljan, lizijantus, margarete, gipsofilu, alstromeriju, dekorativno zelenilo, ukrasni papir i satensku mašnu.",
         to: '/'
     },
     {
         productName: "Šareni tulipani",
         productPrice: "220.00 kn",
         productImage: require("../../src/images/buketi5.jpg"),
+        productDescription: "Buket sadrži tulipane, dekorativno zelenilo, ukrasni papir i satensku mašnu.",
         to: '/' 
     },
     
@@ -77,18 +82,21 @@ export const productsBuketi =[
         productName: "Šareni buket",
         productPrice: "180.00 kn",
         productImage: require("../../src/images/buketi6.jpg"),
+        productDescription:"Buket sadrži lizijantus, dekorativno zelenilo i ukrasni til.",
         to: '/' 
     },
     {
         productName: "Buket Amarilis",
         productPrice: "190.00 kn",
         productImage: require("../../src/images/buketi7.jpg"),
+        productDescription:"Buket sadrži amarilis, gipsofilu, dekorativno zelenilo, ukrasni papir i satensku mašnu.", 
         to: '/'
     },
     {
         productName: "Margarete u rozom",
         productPrice: "170.00 kn",
         productImage: require("../../src/images/buketi8.jpg"),
+        productDescription: "Buket sadrži margarete, gerbere, dekorativno zelenilo, ukrasni til i satensku mašnu.",
         to: '/' 
     }
 ];
@@ -98,42 +106,49 @@ export const productsFlowerboxes = [
         productName: "Cvjetna kutija 'Princeza'",
         productPrice: "450.00 kn",
         productImage: require("../../src/images/flowerbox1.jpg"),
+        productDescription:"15 rozih ruža u kutiji od pliša.",
         to: '/'
     },
     {
         productName: "Cvjetna kutija 'Ruža crvena'",
         productPrice: "400.00 kn",
         productImage: require("../../src/images/flowerbox2.jpg"),
+        productDescription: "13 crvenih ruža i dekorativno zelenilo u kutiji od pliša.",
         to: '/'
     },
     {
         productName: "Cvjetna kutija 'Snježna kraljica'",
         productPrice: "260.00 kn",
         productImage: require("../../src/images/flowerbox3.jpg"),
+        productDescription: " 9 bijelih kala u kutiji od pliša.",
         to: '/'
     },
     {
         productName: "Cvjetna kutija 'Naranča'",
         productPrice: "250.00 kn",
         productImage: require("../../src/images/flowerbox4.jpg"),
+        productDescription: "5 narančastih ruža i 2 grane margareta u kutiji od pliša.",
         to: '/'
     },
     {
         productName: "Cvjetna kutija 'Misterij'",
         productPrice: "400.00 kn",
         productImage: require("../../src/images/flowerbox5.jpg"),
+        productDescription: "11 žutih ruža u kutiji od pliša.",
         to: '/'
     },
     {
         productName: "Cvjetna kutija 'Proljeće'",
         productPrice: "250.00 kn",
         productImage: require("../../src/images/flowerbox6.jpg"),
+        productDescription: "5 rozih ruža i 2 grane lizijantusa u kutiji od pliša.",
         to: '/'
     },
     {
         productName: "Cvjetna kutija siva",
         productPrice: "280.00 kn",
         productImage: require("../../src/images/flowerbox7.jpg"),
+        productDescription: "7 crvenih ruža u kutiji od pliša.", 
         to: '/'
     }
 ];
@@ -142,30 +157,35 @@ export const productsAranzmani = [
         productName: "Nježni aranžman",
         productPrice: "550.00 kn",
         productImage: require("../../src/images/aranzman1.jpg"),
+        productDescription: "Aranžman sadrži ruže, ljiljane, margarete, dekorativno zelenilo i traku za posljednji pozdrav.",
         to: '/'
     },
     {
         productName: "Rozi aranžman",
         productPrice: "500.00 kn",
         productImage: require("../../src/images/aranzman2.jpg"),
+        productDescription: "Aranžman sadrži ruže, ljiljane, margarete, dekorativno zelenilo i traku za posljednji pozdrav.",
         to: '/'
     },
     {
         productName: "Aranžmani ruže i irisi",
         productPrice: "750.00 kn",
         productImage: require("../../src/images/aranzman3.jpg"),
+        productDescription: "Aranžman sadrži ruže, irise, margarete, dekorativno zelenilo i traku za posljednji pozdrav.",
         to: '/'
     },
     {
         productName: "Crveno bijeli aranžman",
         productPrice: "550.00 kn",
         productImage: require("../../src/images/aranzman4.jpg"),
+        productDescription: "Aranžman sadrži ruže, margarete, ljiljan, dekorativno zelenilo i traku za posljednji pozdrav.",
         to: '/'
     },
     {
         productName: "Aranžman crveni gerberi",
         productPrice: "600.00 kn",
         productImage: require("../../src/images/aranzman5.jpg"),
+        productDescription: "Aranžman sadrži gerbere, ruže, margarete, dekorativno zelenilo i traku za posljednji pozdrav.",
         to: '/'
     }
 ];
@@ -174,36 +194,42 @@ export const productsLoncanice = [
         productName: "Monstera",
         productPrice: "110.00 kn",
         productImage: require("../../src/images/loncanice1.jpg"),
+        productDescription: "Biljka jednostavna za održavanje koja potiče kreativnost i opuštanje. Takozvani epifit, koji raste na drugim biljkama kako bi se približio suncu i koji može poboljšati tvoje raspoloženje i učiniti dom življim.",
         to: '/'
     },
     {
         productName: "Agava",
         productPrice: "140.00 kn",
         productImage: require("../../src/images/loncanice2.jpg"),
+        productDescription: "Impozantna izgleda, sadrži brojnu ljekovitu i nutritivnu upotrebu.", 
         to: '/'
     },
     {
         productName: "Fejka",
         productPrice: "220.00 kn",
         productImage: require("../../src/images/loncanice3.jpg"),
+        productDescription: "Umjetna lončanica, nije potrebna njega, viseća 9cm.",
         to: '/'
     },
     {
         productName: "Fikus",
         productPrice: "200.00 kn",
         productImage: require("../../src/images/loncanice4.jpg"),
+        productDescription: "Biljka koja je brzorastuća i kad joj otpadne lišće, lista novo. Može biti osjetljiva na suhoću i promjenu mjesta.", 
         to: '/'
     },
     {
         productName: "Drvo života",
         productPrice: "550.00 kn",
         productImage: require("../../src/images/loncanice5.jpg"),
+        productDescription: "Jedna su od rijetkih biljaka koja uistinu voli sunce. Idealno mjesto za ovu lončanicu je uz prozor ili balkon, terasu. Mogu imati bijelo zvono, odnosno mirisno cvijeće, ali to je vrlo rijetko.",
         to: '/'
     },
     {
         productName: "Yucca",
         productPrice: "300.00 kn",
         productImage: require("../../src/images/loncanice6.jpg"),
+        productDescription: "Biljka jednostavna za održavanje, podnosi suh okoliš.",
         to: '/'
     }
 ];

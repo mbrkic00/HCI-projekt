aab6bac53bb1bfee69ec92e85bc7274935677858
mbrkic00 <mbrkic00.fesb.hr>
2021-02-10 11:13:51 +0100

    adding BlogsContainer style

--- gatsby-project/src/pages/blogs.js
+++ gatsby-project/src/pages/blogs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeaderFooterLayout from '../layouts/headerFooter'
-//import BlogsContainer from '../modules/BlogsContainer'
+import BlogsContainer from '../modules/BlogsContainer'
  
 const Blogs = () => (
   <main>

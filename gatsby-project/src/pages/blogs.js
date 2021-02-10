a25eef5d62db80417d8739dbf70cef7782628dd4
mbrkic00 <mbrkic00.fesb.hr>
2021-02-10 11:10:01 +0100

    adding blogs page

---
+++ gatsby-project/src/pages/blogs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderFooterLayout from '../layouts/headerFooter'
+//import BlogsContainer from '../modules/BlogsContainer'
+ 
+const Blogs = () => (
+  <main>
+    <HeaderFooterLayout activeTab="Blog" >
+    </HeaderFooterLayout>
+  </main>
+)
+ 
+export default Blogs

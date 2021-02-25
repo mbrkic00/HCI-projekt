889dfed7a6e67c9cad2f4604e64073210cf49993
katarinavuknic <katarinavuknic12345@gmail.com>
2021-02-25 18:34:18 +0100

    conflict resolved

--- gatsby-project/src/modules/WebShopBuketiNew/index.js
+++ gatsby-project/src/modules/WebShopBuketiNew/index.js
@@ -117,11 +117,7 @@ const [arrayToShow, setArray] = useState(buketiNiz);
                 </div>
 
             </div> 
-<<<<<<< HEAD
            {/* <ComboBox /> */}
-=======
-    <ComboBox />
->>>>>>> a4197caca2a0dc87d1b61dcb66d52850be4ae864
 
         <li className={styles.list}>
           {data.allContentfulFlowers.nodes.map(node => {
